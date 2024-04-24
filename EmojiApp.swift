80e6e1e0c229f07a32bfd3fb1ddfb2e484c5003f
huaanth <115844975+huaanth@users.noreply.github.com>
2024-04-24 12:51:37 -0400

    Add files via upload
    Manually adding files because I am broke

---
+++ EmojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiApp.swift
+//  Emoji
+//
+//  Created by Anthony Hua on 2024-03-30.
+//
+
+import SwiftUI
+
+@main
+struct EmojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
